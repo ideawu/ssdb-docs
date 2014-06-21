78ee89a32310eb5f436ecf7cdb1c6f7a6d95b5b2
ideawu <ideawu@local>
2014-06-21 09:28:58 +0800

    update

--- src/zh_cn/footer.php
+++ src/zh_cn/footer.php
@@ -2,24 +2,6 @@
 <hr>
 
 
-<div id="disqus_thread"></div>
-<script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = 'ssdb'; // required: replace example with your forum shortname
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-    
-<hr>
-
-
 <div class="footer">
 	Copyright &copy; 2013 - 2014 <a href="http://www.ideawu.com/">ideawu.com</a>. All rights reserved.
 	<?php

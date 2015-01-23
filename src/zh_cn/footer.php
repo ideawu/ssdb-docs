26bd13786eaca8719658e1dfc39d593a7de659ec
ideawu <ideawu@local>
2015-01-23 12:21:07 +0800

    update

--- src/zh_cn/footer.php
+++ src/zh_cn/footer.php
@@ -3,7 +3,7 @@
 
 
 <div class="footer">
-	Copyright &copy; 2013 - 2014 <a href="http://www.ideawu.com/">ideawu.com</a>. All rights reserved.
+	Copyright &copy; 2013 - <?=date('Y')?> <a href="http://www.ideawu.com/">ideawu.com</a>. All rights reserved.
 	<?php
 	$hostname = @exec('hostname');
 	if(strpos($hostname, 'ideawu') !== false){

57d21c34c4f2b92023c2d730e74b7add880a4b4a
ideawu <ideawu@local>
2015-02-08 12:07:29 +0800

    update

--- src/zh_cn/footer.php
+++ src/zh_cn/footer.php
@@ -4,6 +4,7 @@
 
 <div class="footer">
 	Copyright &copy; 2013 - <?=date('Y')?> <a href="http://www.ideawu.com/">ideawu.com</a>. All rights reserved.
+	<?php echo 'Updated: ' . date('Y-m-d H:i:s O'); ?>
 	<?php
 	$hostname = @exec('hostname');
 	if(strpos($hostname, 'ideawu') !== false){
